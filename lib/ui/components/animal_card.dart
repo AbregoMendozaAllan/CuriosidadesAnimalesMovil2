72f78f8d2801e057e6960de809f93aaf589d5c24
AbregoMendozaAllan <153949305+AbregoMendozaAllan@users.noreply.github.com>
2024-04-07 14:08:21 -0600

    Creacion de animal_card.dart y se agrego a main.dart solo para ver como se ve en ejecucion.

---
+++ lib/ui/components/animal_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AnimalCard extends StatelessWidget {
+  final String name;
+  final String description;
+  final String emoji;
+
+  const AnimalCard({
+    super.key,
+    required this.name,
+    required this.description,
+    required this.emoji,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // To make the card wrap its content
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8.0), // For spacing
+            Text(
+              description,
+              style: const TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(height: 8.0), // For spacing
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                emoji,
+                style: const TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
