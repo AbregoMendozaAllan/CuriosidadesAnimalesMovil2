1a2d97e9a84fdad76b2606dca6b2631c4203dceb
AbregoMendozaAllan <153949305+AbregoMendozaAllan@users.noreply.github.com>
2024-04-07 22:41:56 -0600

    navigation between home and nueva curiosidad functional

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
 import 'components/animal_card.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0; // Default index of first screen
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end, // Align content to the bottom
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
                   padding: EdgeInsets.only(left: 16.0, bottom: 10.0),
