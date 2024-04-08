79acf583bcd619917e2074605bf1cc6a8ebfbdaf
AbregoMendozaAllan <153949305+AbregoMendozaAllan@users.noreply.github.com>
2024-04-07 22:40:36 -0600

    navigation between home and nueva curiosidad functional

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:curiosidadesanimalesmovil2/ui/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
